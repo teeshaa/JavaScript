b296ec3a66420cb1604f57fb0c52d74c476c42d6
Teesha Ghevariya <tishaghevariya@gmail.com>
2024-01-08 12:17:47 +0530

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12
+let accountEmail = "tishaghevariya@gmail.com"
+var accountPassword = '1213'
+accountCity = "Jaipur"
+
+console.log(accountId);
+
+// prefer not to use var because of issue in 
+// block scope and functional scope 
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const { log } = require("console");
+
+// alert(3 + 3) we are using node js, not browser 
+
+console.log(3 + 3); 
+console.log("Teesha") // code readability 
+// documentation mdn 
+
+let name = "teesha"
+let age = 20
+let isLoggedIn = false 
+
+// number => 2 to power 53
+// bigint 
+
