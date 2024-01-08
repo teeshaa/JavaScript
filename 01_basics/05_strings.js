c9e80c8c40ac9ac62c6412ecd16c53859db675bd
Teesha Ghevariya <tishaghevariya@gmail.com>
2024-01-08 16:19:41 +0530

    Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+const name = "teesha "
+const repoCount = 50
+
+// console.log(typeof name);
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+const gameName = new String('teesha-tg-com')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(0, 8)
+console.log(anotherString);
+
+const newStringOne = " Teesha "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "https://teesha.com/teesha%20ghevariya"
+console.log(url.replace('%20','-'));
+console.log(url.includes('teesha'));
+
+console.log(gameName.split('-'));
