8e1088cce8ffb080f4c77b64a4c6e05ef478af9a
Teesha Ghevariya <tishaghevariya@gmail.com>
2024-01-08 12:33:05 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,22 @@
+let score = "33hgf"
+ 
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof (valueInNumber));
+// console.log(valueInNumber)
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false;
+// "" => false 
+// "teesha" => false
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
