d850cefad8ee4bf9fcccf9e9a8c96829fd7cebee
Teesha Ghevariya <tishaghevariya@gmail.com>
2024-01-11 09:44:30 +0530

    Loops

---
+++ while.js
@@ -0,0 +1,22 @@
+// While loop 
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2 
+// }
+
+// let myArray = ["superman", "teesha", "tishaa"]
+
+// let arr = 0
+// while (arr< myArray.length) {
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr = arr + 1  
+// }
+
+
+let score = 12
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
